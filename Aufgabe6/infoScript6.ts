8713af0c73f628335934653f28fe607882336c37
LaraMMue <72947813+LaraMMue@users.noreply.github.com>
2020-11-25 21:06:30 +0100

    in eine Funktion zusammegefasst (mit austauschbaren Werten)

--- Aufgabe6/infoScript6.ts
+++ Aufgabe6/infoScript6.ts
@@ -33,119 +33,41 @@ var Australia18: number = 2100.5
 
 var EmissionGes: number = Europe18 + nAmerica18 + sAmerica18 + Africa18 + Asia18 + Australia18;
 
-// Berechnungen
 
-/* Europa */
-var EuropeProzentWelt : string = ((Europe18/EmissionGes)*100).toFixed(2);
-var EuropeVerglProzent : string = (((Europe18/Europe08)-1)*100).toFixed(2);
-var EuropeVerglZahl: string = (Europe18-Europe08).toFixed(2);
+/* allgemeine Funktion */
 
-/* Nordamerika */
-var nAmericaProzentWelt : string = ((nAmerica18/EmissionGes)*100).toFixed(2);
-var nAmericaVerglProzent : string = (((nAmerica18/nAmerica08)-1)*100).toFixed(2);
-var nAmericaVerglZahl : string = (nAmerica18-nAmerica08).toFixed(2);
+function continentValues(continent:string, wert08:number, wert18:number) {
+    document.querySelector("#titleRegion").innerHTML= continent;
+    document.querySelector("#emAbRegion").innerHTML= continent;
+    document.querySelector("#emissionAbsolute").innerHTML= wert18 + "kg CO2";
+    document.querySelector("#emissionRelative").innerHTML= ((wert18/EmissionGes)*100).toFixed(2) + "%";
+    document.querySelector("#growthRatePercent").innerHTML= (((wert18/wert08)-1)*100).toFixed(2) + "%";
+    document.querySelector("#growthRateAbsolute").innerHTML= (wert18-wert08).toFixed(2) + "kg CO2";
 
-/* Südamerika */
-var sAmericaProzentWelt: string = ((sAmerica18/EmissionGes)*100).toFixed(2);
-var sAmericaVerglProzent: string = (((sAmerica18/sAmerica08)-1)*100).toFixed(2);
-var sAmericaVerglZahl: string = (sAmerica18-sAmerica08).toFixed(2);
-
-/* Afrika */
-var AfricaProzentWelt: string = ((Africa18/EmissionGes)*100).toFixed(2);
-var AfricaVerglProzent: string = (((Africa18/Africa08)-1)*100).toFixed(2);
-var AfricaVerglZahl: string = (Africa18-Africa08).toFixed(2);
-
-/* Asien */
-var AsiaProzentWelt: string = ((Asia18/EmissionGes)*100).toFixed(2);
-var AsiaVerglProzent: string = (((Asia18/Asia08)-1)*100).toFixed(2);
-var AsiaVerglZahl: string = (Asia18-Asia08).toFixed(2);
-
-/* Australien */
-var AustraliaProzentWelt: string = ((Australia18/EmissionGes)*100).toFixed(2);
-var AustraliaVerglProzent: string = (((Australia18/Australia08)-1)*100).toFixed(2);
-var AustraliaVerglZahl: string = (Australia18-Australia08).toFixed(2);
-
-/* ----------------------------------------------------------------- */
-
-/* Funktion Europa */
-function Europa () {
-    document.getElementById ("titleRegion").innerHTML="Europe";
-    document.getElementById ("emAbRegion").innerHTML="Europe";
-    document.getElementById ("emissionAbsolute").innerHTML= Europe18 + "kg CO2";
-    document.getElementById ("emissionRelative").innerHTML= EuropeProzentWelt + "%";
-    document.getElementById ("growthRatePercent").innerHTML= EuropeVerglProzent + "%";
-    document.getElementById ("growthRateAbsolute").innerHTML= EuropeVerglZahl + "kg CO2";
-
-    document.querySelector (".chart").setAttribute ("style", "height:" + EuropeProzentWelt + "%");
-
-
+    document.querySelector(".chart").setAttribute("style", "height:" + ((wert18/EmissionGes)*100) + "%");
 }
-document.querySelector (".europe").addEventListener ('click', Europa);
 
-/* Funktion Nordamerika */
-function Nordamerika () {
-    document.getElementById ("titleRegion").innerHTML="North America";
-    document.getElementById ("emAbRegion").innerHTML= "North America";
-    document.getElementById ("emissionAbsolute").innerHTML= nAmerica18 + "kg CO2";
-    document.getElementById ("emissionRelative").innerHTML= nAmericaProzentWelt + "%";
-    document.getElementById ("growthRatePercent").innerHTML= nAmericaVerglProzent + "%";
-    document.getElementById ("growthRateAbsolute").innerHTML= nAmericaVerglZahl + "kg CO2";
+/* Events */
+document.querySelector(".europe").addEventListener("click", function() {
+    continentValues("Europe", Europe08, Europe18);
+})
 
-    document.querySelector (".chart").setAttribute ("style", "height:" + nAmericaProzentWelt + "%");
-}
-document.querySelector (".northamerica").addEventListener ('click', Nordamerika);
+document.querySelector(".northamerica").addEventListener("click", function() {
+    continentValues("North America", nAmerica08, nAmerica18);
+})
 
-/* Funktion Südamerika */
-function SouthAmerica () {
-    document.getElementById ("titleRegion").innerHTML="South America";
-    document.getElementById ("emAbRegion").innerHTML="South America";
-    document.getElementById ("emissionAbsolute").innerHTML= sAmerica18 + "kg CO2";
-    document.getElementById ("emissionRelative").innerHTML= sAmericaProzentWelt + "%";
-    document.getElementById ("growthRatePercent").innerHTML= sAmericaVerglProzent + "%";
-    document.getElementById ("growthRateAbsolute").innerHTML= sAmericaVerglZahl + "kg CO2";
+document.querySelector(".southamerica").addEventListener("click", function() {
+    continentValues("South America", sAmerica08, sAmerica18);
+})
 
-    document.querySelector (".chart").setAttribute ("style", "height:" + sAmericaProzentWelt + "%");
-}
-document.querySelector (".southamerica").addEventListener ('click', SouthAmerica);
+document.querySelector(".africa").addEventListener("click", function() {
+    continentValues("Africa", Africa08, Africa18);
+})
 
-/* Funktion Afrika */
-function Africa () {
-    document.getElementById ("titleRegion").innerHTML="Africa";
-    document.getElementById ("emAbRegion").innerHTML="Africa";
-    document.getElementById ("emissionAbsolute").innerHTML= Africa18 + "kg CO2";
-    document.getElementById ("emissionRelative").innerHTML= AfricaProzentWelt + "%";
-    document.getElementById ("growthRatePercent").innerHTML= AfricaVerglProzent + "%";
-    document.getElementById ("growthRateAbsolute").innerHTML= AfricaVerglZahl + "kg CO2";
-    
-    document.querySelector (".chart").setAttribute ("style", "height:" + AfricaProzentWelt + "%");
+document.querySelector(".asia").addEventListener("click", function() {
+    continentValues("Asia", Asia08, Asia18);
+})
 
-}
-document.querySelector (".africa").addEventListener ('click', Africa);
-
-/* Funktion Asien */
-function Asia () {
-    document.getElementById ("titleRegion").innerHTML="Asia";
-    document.getElementById ("emAbRegion").innerHTML="Asia";
-    document.getElementById ("emissionAbsolute").innerHTML= Asia18 + "kg CO2";
-    document.getElementById ("emissionRelative").innerHTML= AsiaProzentWelt + "%";
-    document.getElementById ("growthRatePercent").innerHTML= AsiaVerglProzent + "%";
-    document.getElementById ("growthRateAbsolute").innerHTML= AsiaVerglZahl + "kg CO2";
-
-    document.querySelector (".chart").setAttribute ("style", "height:" + AsiaProzentWelt + "%");
-
-}
-document.querySelector (".asia").addEventListener ('click', Asia);
-
-/* Funktion Australien */
-function Australia () {
-    document.getElementById ("titleRegion").innerHTML="Australia";
-    document.getElementById ("emAbRegion").innerHTML="Australia";
-    document.getElementById ("emissionAbsolute").innerHTML= Australia18 + "kg CO2";
-    document.getElementById ("emissionRelative").innerHTML= AustraliaProzentWelt + "%";
-    document.getElementById ("growthRatePercent").innerHTML= AustraliaVerglProzent + "%";
-    document.getElementById ("growthRateAbsolute").innerHTML= AustraliaVerglZahl + "kg CO2";
-
-    document.querySelector (".chart").setAttribute ("style", "height:" + AustraliaProzentWelt + "%");
-
-}
-document.querySelector (".australia").addEventListener ('click', Australia);
+document.querySelector(".australia").addEventListener("click", function() {
+    continentValues("Australia", Australia08, Australia18);
+})
